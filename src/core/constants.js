13d032d837744b47b0881e02b20d9f1329015e87
leny <info@flatland.be>
2020-05-20 12:22:21 +0200

    Use correct values for constants

--- src/core/constants.js
+++ src/core/constants.js
@@ -6,7 +6,7 @@
  * started at 20/05/2020
  */
 
-// export const SESSION_DURATION = 1500; // 25min
-export const SESSION_DURATION = 3; // 1min 30
+export const SESSION_DURATION = 1500; // 25min
+export const PAUSE_DURATION = 300; // 5min
 
 export const NBSP = "\u00a0";
